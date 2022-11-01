c8a10b7bfb90fcf5aadc39312a966d995eca8426
KuOcWh <kdoescoding@gmail.com>
2022-11-01 15:59:36 +0000

    'coding question'

--- CodingChallenges/eightKyuChallenges/app1_11_2022.js
+++ CodingChallenges/eightKyuChallenges/app1_11_2022.js
@@ -5,3 +5,8 @@ function sumTwoSmallestNumbers(numbers) {
     let sorted = numbers.sort((a, b) => a - b)
     return sorted[0] + sorted[1]
   }
+
+
+  //Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+//Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
