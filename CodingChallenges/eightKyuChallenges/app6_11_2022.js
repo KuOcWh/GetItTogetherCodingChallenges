53b27a7b65dbb1a74c521b2816a21f0d0c5e75b2
KuOcWh <kdoescoding@gmail.com>
2022-11-08 15:18:23 +0000

    'another coding challenge'

--- CodingChallenges/eightKyuChallenges/app6_11_2022.js
+++ CodingChallenges/eightKyuChallenges/app6_11_2022.js
@@ -8,3 +8,6 @@ var summation = function (num) {
     }
     return total
   }
+
+
+ // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
