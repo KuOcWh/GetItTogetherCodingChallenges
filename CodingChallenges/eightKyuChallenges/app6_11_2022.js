af805c410f952998c255e051a5325ece78dd8a3d
KuOcWh <kdoescoding@gmail.com>
2022-11-08 15:18:45 +0000

    'question answered'

--- CodingChallenges/eightKyuChallenges/app6_11_2022.js
+++ CodingChallenges/eightKyuChallenges/app6_11_2022.js
@@ -11,3 +11,12 @@ var summation = function (num) {
 
 
  // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+ function solution(str, ending){
+  // TODO: complete
+  if(str.endsWith(ending)){
+    return true
+  }else{
+    return false
+  }
+}
